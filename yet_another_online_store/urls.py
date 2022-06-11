7d54396d30f695f91de500b9388afabd6b0777f0
Khalanar <pablo.xez@gmail.com>
2022-06-11 17:37:46 +0000

    fixed an issue where 404 pages were not being displayed properly

--- yet_another_online_store/urls.py
+++ yet_another_online_store/urls.py
@@ -30,4 +30,4 @@ urlpatterns = [
     path('profiles/', include('profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-handler404 = 'boutique_ado.views.handler404'
+handler404 = 'yet_another_online_store.views.handler404'
