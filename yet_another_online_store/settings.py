dfa21373422c7c6a5a0797d741347afe500a7ccb
Khalanar <pablo.xez@gmail.com>
2022-06-11 15:38:13 +0000

    remove secret keys

--- yet_another_online_store/settings.py
+++ yet_another_online_store/settings.py
@@ -26,10 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3&!aucymuq8+@bxo8y!^16+3!02q5e2up^(z@(2q)d4!d^)^7l'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['yaostore.herokuapp.com', 'localhost']
 
